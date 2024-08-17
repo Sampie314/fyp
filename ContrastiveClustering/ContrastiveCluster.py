1e35a7984ef24a42b7268665af1b075340ae0a61
Sampie314 <samuelcwk@hotmail.com>
2024-08-17 23:45:00 +0800

    fix small bug

--- ContrastiveClustering/ContrastiveCluster.py
+++ ContrastiveClustering/ContrastiveCluster.py
@@ -269,7 +269,7 @@ class ResNetFashionMNIST(nn.Module):
     def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
                  groups=1, width_per_group=64, replace_stride_with_dilation=None,
                  norm_layer=None):
-        super(ResNet, self).__init__()
+        super(ResNetFashionMNIST, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         self._norm_layer = norm_layer
