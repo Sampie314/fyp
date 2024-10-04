669e723f1d8a651bad5f256fc62442323d8704ed
Sampie314 <74698833+Sampie314@users.noreply.github.com>
2024-10-04 22:07:39 +0800

    added RL utils

---
+++ RL/utils/preprocessors.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def reindex(df: pd.DataFrame, date_col: str = 'Date', ticker_col:str = 'symbol') -> pd.DataFrame:
+    """reindex and sort the dataframe to prepare for RLenv"""
+    df = df.copy()
+    df = df.sort_values([ticker_col, date_col], ignore_index=True)
+    df.index = df[ticker_col].factorize()[0]
+    return df
