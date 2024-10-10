52632c55ae1e0dc41cab2fa1f48cfff207798737
#CHIA WEI KIT <chia0427@CCDS-TC1.cm.cluster>
2024-10-11 01:20:31 +0800

    update

--- runpythonscript.sh
+++ runpythonscript.sh
@@ -4,14 +4,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=10
 #SBATCH --nodelist=TC1N04
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:0
 #SBATCH --mem=64G
 #SBATCH --time=360
-#SBATCH --job-name=agglo_res
+#SBATCH --job-name=HDBSCAN_cluster_6
 #SBATCH --output=%x_output_%j.out
 #SBATCH --error=%x_error_%j.err
 
 module load anaconda
 source activate fyp1
-python Agglomerative_res.py
+python HDBSCAN_cluster_6.py
 
